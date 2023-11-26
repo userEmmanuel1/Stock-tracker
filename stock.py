19bfdeed479ae35581f8109f2664f3d74f78b4a6
userEmmanuel1 <146595738+userEmmanuel1@users.noreply.github.com>
2023-11-25 22:47:04 -0800

    Update stock.py
    cleaned up with chatgpt, still gonna change the formula to determine selling price on stocks

--- stock.py
+++ stock.py
@@ -1,65 +1,72 @@
 import requests
-import time 
+import time
 import smtplib
-import ssl 
-#Email sender 
-smtp_port= 587
+import ssl
+
+# Email sender
+smtp_port = 587
 smtp_server = "smtp.gmail.com"
 
-#To and from emails
-email_from= "ecueva003@gmail.com"
-email_to= "ecueva003@gmail.com"
+# To and from emails
+email_from = "ecueva003@gmail.com"
+email_to = "ecueva003@gmail.com" 
 
-pw=""
-        
+pw = ""
+
 simple_email_context = ssl.create_default_context()
-#Stock tracker 
 
-#Choosing the specific stock using the 3-4 letters
+# Stock tracker
+
+# Choosing the specific stock using the 3-4 letters
 ticker = input(f"Choose a stock to follow: ")
-msg= f"Check the stock{ticker}"
+msg1 = f"Check the stock {ticker}, probably SELL"
 api_key = "71959a3173904d4cb215fe33857b2665"
 
 
-#Basic information about the stock
-def get_stock_price(ticker_symbol,api):
-    url=f"https://api.twelvedata.com/price?symbol={ ticker}&apikey={api}"
-    url2=f"https://api.twelvedata.com/quote?symbol={ticker}&apikey={api}"
-    response =requests.get(url).json()
-    price = response['price']
+def get_stock_price(ticker_symbol, api):
+    url = f"https://api.twelvedata.com/price?symbol={ticker}&apikey={api}"
+    url2 = f"https://api.twelvedata.com/quote?symbol={ticker}&apikey={api}"
+    response = requests.get(url).json()
+    price = response['price'][:-3]
     print("$" + price + " per share for " + ticker_symbol)
-get_stock_price(ticker, api_key)
-def get_stock_price(ticker_symbol,api):
-    url2=f"https://api.twelvedata.com/quote?symbol={ticker}&apikey={api}"    
-    response2 = requests.get(url2).json()
-    low_price=response2['low']
-    high_price=response2['high']
-    open_price = response2['open']
-    close_price=response2['close']
-         
-    if open_price != close_price: 
-        try:
-            TIE_server = smtplib.SMTP(smtp_server, smtp_port)
-            TIE_server.starttls(context=simple_email_context)
-            TIE_server.login(email_from, pw)
-     
-            print (f"Sending email to {email_to}")
-            TIE_server.sendmail(email_from, email_to, msg)
-            print(f"Sent email to {email_to}:")
+    return price
 
 
-            import requests
-            resp = requests.post('https://textbelt.com/text', {
+def send_notification(message):
+    try:
+        TIE_server = smtplib.SMTP(smtp_server, smtp_port)
+        TIE_server.starttls(context=simple_email_context)
+        TIE_server.login(email_from, pw)
+
+        print(f"Sending email to {email_to}")
+        TIE_server.sendmail(email_from, email_to, message)
+        print(f"Sent email to {email_to}")
+ 
+        resp = requests.post('https://textbelt.com/text', {
             'phone': '+13237173041',
             'heading': 'STFU',
-            'message': f'Sell the stock {ticker}',
+            'message': message,
             'key': 'cd2595248c0ac46913b132b3ff6f335fa996cd95BAfjSt35FbB3Nnp0X7oxtIJlE',
-            })
-            print(resp.json())
+        })
+        print(resp.json())
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        TIE_server.quit()
 
 
-        except Exception as e:
-            print(f"Dumbass failed lmao{e}")
+# Basic information about the stock
+price = get_stock_price(ticker, api_key)
+low_price = high_price = open_price = close_price = float(price)
 
-        finally:
-            TIE_server.quit()
+sell_price1 = open_price + close_price
+sell_price2 = open_price - close_price
+
+while sell_price1 or sell_price2 == True:
+    if sell_price1 or sell_price2 == True:
+        send_notification(msg1)
+    else:
+        print("Don't touch the stock")
+    time.sleep(5)  # Adjust the sleep time based on your needs
